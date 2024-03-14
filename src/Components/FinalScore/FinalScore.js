b6d0a04e1b568e2c18ca69092a8c6598eb27440e
georgeharvey3 <georgeharvey3@hotmail.com>
2024-03-14 17:27:44 +0000

    name placeholder

--- src/Components/FinalScore/FinalScore.js
+++ src/Components/FinalScore/FinalScore.js
@@ -2,7 +2,7 @@ const finalScore = (props) => (
   <div>
     <h2>You scored {props.score} points</h2>
     <form onSubmit={props.onScoreFormSubmit}>
-      <input className="name-input" name="score" value={props.nameInputValue} onChange={props.onNameInputChange}/>
+      <input placeholder="Name..." className="name-input" name="score" value={props.nameInputValue} onChange={props.onNameInputChange}/>
       <input type="submit" value="Save Score" />
     </form>
   </div>
